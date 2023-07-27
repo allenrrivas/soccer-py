f24db094e0677d0beeee6a2a08c83b2e3de35048
allenrrivas <allenrrivas.30@gmail.com>
2023-07-26 17:25:40 -0700

    Added shot on post, save shots, blocked shots, and missed shots.

--- ViniciusJrShotMap.py
+++ ViniciusJrShotMap.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
-from mplsoccer import VerticalPitch
 import numpy as np
 import pandas as pd
+from highlight_text import fig_text
+from mplsoccer import VerticalPitch
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -39,15 +40,28 @@ xGcum = np.round(max(np.cumsum(shots['xG'])),3).tolist()
 xG_per_shot = np.round(max(np.cumsum(shots['xG']))/(shots[shots.columns[0]].count()),3).tolist()
 goal = shots[shots['result']=='Goal']
 shot_on_post = shots[shots['result']=='ShotOnPost']
-blocked_shot = shots[shots['result']=='BlockedShot']
 saved_shot = shots[shots['result']=='SavedShot']
-missed_shot = shots[shots['result']=='MissedShot']
+blocked_shot = shots[shots['result']=='BlockedShot']
+missed_shot = shots[shots['result']=='MissedShots']
 goals = goal[goal.columns[0]].count().tolist()
 
 pitch = VerticalPitch(half=True, pitch_type='opta', pitch_color='#22312b', line_color='#ffffff', axis=True, label=True, tick=True)
 fig, ax = pitch.draw(figsize=(12, 9))
 
-plt.scatter(goal['Y'], goal['X'], s=(goal["xG"]* 720) + 100, c='#90ee90', alpha=.7)
-plt.title("Vinícius Júnior", y=0.3, fontsize=40, color='white')
+plt.scatter(goal['Y'], goal['X'], s=(goal["xG"]* 720) + 100, c='#2ecc71', alpha=.7)
+plt.scatter(shot_on_post['Y'], shot_on_post['X'], s=shot_on_post["xG"]* 720, c='#f1c40f', alpha=.7)
+plt.scatter(saved_shot['Y'], saved_shot['X'], s=saved_shot["xG"]* 720, c='#3498db', alpha=.7)
+plt.scatter(blocked_shot['Y'], blocked_shot['X'], s=blocked_shot["xG"]* 720, c='#9b59b6', alpha=.7)
+plt.scatter(missed_shot['Y'], missed_shot['X'], s=(missed_shot["xG"]* 720), c='#e74c3c', alpha=.7)
+
+
+s='<Vinícius Júnior Career Goals>'
+highlight_textprops =[{"color": "#ffffff"}]
+fig_text(s=s,
+        x=0.27,y=0.4,
+        highlight_textprops=highlight_textprops,
+        fontname='monospace',
+        fontsize=24
+)
 
 plt.show()
